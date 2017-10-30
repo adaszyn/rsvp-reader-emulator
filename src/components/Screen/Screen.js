6bdcf824246842fedf475012566bc7006cc5fb23
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-10-30 20:03:58 +0100

    Interval setup inside Screen component

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { splitTextByWordCount } from "../../util/text-util";
 import "./Screen.css";
 
 export default class Screen extends Component {
   constructor(props) {
+    const { text, wordsPerScreen } = props;
     super(props);
     this.state = {
-    }
+      words: splitTextByWordCount(text, wordsPerScreen),
+      currentWordIndex: 0
+    };
+    this.setupInterval();
+  }
+  setupInterval () {
+    this.intervalId = setInterval(() => {
+      if (this.state.currentWordIndex === this.state.words.length) {
+        clearInterval(this.intervalId);
+      }
+      this.setState({
+        currentWordIndex: this.state.currentWordIndex + 1
+      })
+    }, this.props.speed)
   }
   render() {
     const { width, height } = this.props;
-    return <div style={{ height, width }} className="screen" />;
+    return <div style={{ height, width }} className="screen">
+      <span>{this.state.words[this.state.currentWordIndex]}</span>
+    </div>;
   }
 }
 
