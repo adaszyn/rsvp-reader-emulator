e800ba7f86f2739d181825abffb9053f1ff5dbd8
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-10-30 20:04:38 +0100

    Sample text from App component passed to Screen component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,20 @@
-import React, { Component } from 'react';
-import Screen from '../Screen/Screen';
-import './App.css';
+import React, { Component } from "react";
+import Screen from "../Screen/Screen";
+import "./App.css";
 
+const SAMPLE_TEXT = `Lorem ipsum dolor sit amet, consectetur 
+adipiscing elit, sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis`;
+
+function onStartCallback (startTime) {
+
+
+}
+function onStopCallback (stopTime) {
+
+}
+  
 class App extends Component {
   render() {
     return (
@@ -9,8 +22,11 @@ class App extends Component {
         <Screen
           width={300}
           height={300}
-          wordsPerScreen={1}
-          speed={30}
+          wordsPerScreen={2}
+          speed={800}
+          text={SAMPLE_TEXT}
+          onStart={onStartCallback}
+          onStop={onStopCallback}
         />
       </div>
     );
