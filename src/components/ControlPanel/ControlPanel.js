f39f1a4b41c0129c5e3769925b2a07e376d2d984
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-11-19 15:35:08 +0100

    Speed selection in ControlPanel component with input:range

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import PlayIcon from '../Icons/PlayIcon';
-import ResetIcon from '../Icons/ResetIcon';
+import PlayIcon from "../Icons/PlayIcon";
+import ResetIcon from "../Icons/ResetIcon";
 import "./ControlPanel.css";
 
-
-
 export default class ControlPanel extends Component {
+  constructor() {
+    super();
+    this.state = {
+      speed: 100
+    }
+  }
+  onSpeedChange({ target: { value } }) {
+    this.props.onSpeedChange(value);
+    this.setState({
+      speed: value
+    })
+  }
   render() {
     const { onReset, onStart, onSpeedChange, running } = this.props;
     return (
@@ -16,12 +26,17 @@ export default class ControlPanel extends Component {
         ) : (
           <PlayIcon onStart={onStart} />
         )}
-        <div className="speed-select-container">
-          <select disabled={running} className="speed-select" onChange={({target: {value}}) => onSpeedChange(value)}>
-            <option>400</option>
-            <option>800</option>
-            <option>1200</option>
-          </select>
+        <div>
+          <span style={{color: "white"}}>{this.state.speed}</span>
+          <br/>
+          <input
+            type="range"
+            min={100}
+            defaultValue={100}
+            max={1000}
+            disabled={running}
+            onChange={this.onSpeedChange.bind(this)}
+          />
         </div>
       </div>
     );
